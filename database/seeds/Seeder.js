a334e78c9d75b848a53537f7b30eb3c11708b2c3
hdinjos <hdinjos@gmail.com>
2021-08-04 22:58:41 +0700

    add seeder

--- database/seeds/Seeder.js
+++ database/seeds/Seeder.js
@@ -1,14 +1,36 @@
-'use strict'
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const Database = use('Database')
+const fs = require("fs");
+const path = require("path");
+const Database = use("Database");
 
 class Seeder {
   async run() {
-    await Database.raw(fs.readFileSync(path.join(__dirname, 'data', 'authors.sql')).toString());
-    await Database.raw(fs.readFileSync(path.join(__dirname, 'data', 'articles.sql')).toString());
+    await Database.raw(
+      fs.readFileSync(path.join(__dirname, "data", "authors.sql")).toString()
+    );
+    await Database.raw(
+      fs.readFileSync(path.join(__dirname, "data", "articles.sql")).toString()
+    );
+    await Database.raw(
+      fs.readFileSync(path.join(__dirname, "data", "knnresults.sql")).toString()
+    );
+    await Database.raw(
+      fs.readFileSync(path.join(__dirname, "data", "modes.sql")).toString()
+    );
+    await Database.raw(
+      fs.readFileSync(path.join(__dirname, "data", "precisions.sql")).toString()
+    );
+    await Database.raw(
+      fs.readFileSync(path.join(__dirname, "data", "recalls.sql")).toString()
+    );
+    await Database.raw(
+      fs.readFileSync(path.join(__dirname, "data", "akurasis.sql")).toString()
+    );
+    await Database.raw(
+      fs.readFileSync(path.join(__dirname, "data", "f_1_s.sql")).toString()
+    );
   }
 }
 
-module.exports = Seeder
+module.exports = Seeder;
