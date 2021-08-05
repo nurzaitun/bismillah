c6533afc9739a62090b51fe6bf9091692422ff30
hdinjos <hdinjos@gmail.com>
2021-08-05 18:19:42 +0700

    change seeder

--- database/seeds/Seeder.js
+++ database/seeds/Seeder.js
@@ -6,30 +6,30 @@ const Database = use("Database");
 
 class Seeder {
   async run() {
-    await Database.raw(
-      fs.readFileSync(path.join(__dirname, "data", "authors.sql")).toString()
-    );
-    await Database.raw(
-      fs.readFileSync(path.join(__dirname, "data", "articles.sql")).toString()
-    );
-    await Database.raw(
-      fs.readFileSync(path.join(__dirname, "data", "knnresults.sql")).toString()
-    );
+    // await Database.raw(
+    //   fs.readFileSync(path.join(__dirname, "data", "authors.sql")).toString()
+    // );
+    // await Database.raw(
+    //   fs.readFileSync(path.join(__dirname, "data", "articles.sql")).toString()
+    // );
+    // await Database.raw(
+    //   fs.readFileSync(path.join(__dirname, "data", "knnresults.sql")).toString()
+    // );
     await Database.raw(
       fs.readFileSync(path.join(__dirname, "data", "modes.sql")).toString()
     );
-    await Database.raw(
-      fs.readFileSync(path.join(__dirname, "data", "precisions.sql")).toString()
-    );
-    await Database.raw(
-      fs.readFileSync(path.join(__dirname, "data", "recalls.sql")).toString()
-    );
-    await Database.raw(
-      fs.readFileSync(path.join(__dirname, "data", "akurasis.sql")).toString()
-    );
-    await Database.raw(
-      fs.readFileSync(path.join(__dirname, "data", "f_1_s.sql")).toString()
-    );
+    // await Database.raw(
+    //   fs.readFileSync(path.join(__dirname, "data", "precisions.sql")).toString()
+    // );
+    // await Database.raw(
+    //   fs.readFileSync(path.join(__dirname, "data", "recalls.sql")).toString()
+    // );
+    // await Database.raw(
+    //   fs.readFileSync(path.join(__dirname, "data", "akurasis.sql")).toString()
+    // );
+    // await Database.raw(
+    //   fs.readFileSync(path.join(__dirname, "data", "f_1_s.sql")).toString()
+    // );
   }
 }
 
